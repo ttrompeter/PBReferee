5733b1854a6366027c76922d50caf07a75f10e07
Tom Trompeter <63081891+ttrompeter@users.noreply.github.com>
2022-09-23 16:18:33 -0700

    Initial Commit

---
+++ PBReferee/PBRefereeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PBRefereeApp.swift
+//  PBReferee
+//
+//  Created by Tom Trompeter on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct PBRefereeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
